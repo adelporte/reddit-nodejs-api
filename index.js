47532a3c720fcc3c375fb4d90950d7b8c9f6d784
Augustin Delporte <delporteaugustin@gmail.com>
2016-04-28 21:20:04 +0000

    Finished the exercises before subreddit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 // create a connection to our Cloud9 server
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'ziad_saab', // CHANGE THIS :)
+  user     : 'adelporte', // CHANGE THIS :)
   password : '',
   database: 'reddit'
 });
@@ -13,26 +13,32 @@ var connection = mysql.createConnection({
 var reddit = require('./reddit');
 var redditAPI = reddit(connection);
 
-// It's request time!
-redditAPI.createUser({
-  username: 'hello23',
-  password: 'xxx'
-}, function(err, user) {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    redditAPI.createPost({
-      title: 'hi reddit!',
-      url: 'https://www.reddit.com',
-      userId: user.id
-    }, function(err, post) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log(post);
-      }
-    });
-  }
-});
+//It's request time!
+// redditAPI.createUser({
+//   username: 'hello232',
+//   password: 'xxx'
+// }, function(err, user) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   else {
+//     redditAPI.createPost({
+//       title: 'hi reddit!',
+//       url: 'https://www.reddit.com',
+//       userId: user.id
+//     }, function(err, post) {
+//       if (err) {
+//         console.log(err);
+//       }
+//       else {
+//         console.log(post);
+//       }
+//     });
+//   }
+// });
+
+// redditAPI.getAllPosts(function(err, result){console.log(result)});
+
+//redditAPI.getAllPostsForUser(1, function(err, result){console.log(result)})
+
+redditAPI.getSinglePost(1, function(err, result){console.log(result)});
